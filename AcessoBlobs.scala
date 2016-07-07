5ff0b79b097b66325f9cffa80c6f9b8e74eadb47
vinicius-alves <vinicius.alves@poli.ufrj.br>
2016-07-07 15:19:52 -0300

    Delete AcessoBlobs.scala

--- AcessoBlobs.scala
+++
@@ -1,2 +0,0 @@
-//Acessando arquivos de outro blob conteiner = <main container> storagegrupocluster = < account > 2015.csv = <path>
-val myrdd = sc.textFile("wasb://conteiner@storagegrupocluster.blob.core.windows.net/2015.csv");
