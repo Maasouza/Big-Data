ca45909a4bddc750ecf41bfb39d0c4a05ffe38bc
vinicius-alves <vinicius.alves@poli.ufrj.br>
2016-07-07 15:16:52 -0300

    Update alternative.scala

--- CSVToDataFrame/alternative.scala
+++ CSVToDataFrame/alternative.scala
@@ -1,24 +1,29 @@
 import java.sql.Timestamp
 import sqlContext.implicits._
 
+//Modelo de registro para a tabela de saída
 case class Register(Estacao:String,Data:Timestamp,Tipo:String,Valor:Short,Latitude:Float,Longitude:Float);
 
-val station = sc.textFile("wasb://datasets@storagegrupocluster.blob.core.windows.net/ghcnd-stations.txt").
+//Abre o arquivo que contém as estações, latitude e longitude de cada registro, o transforma em uma RDD
+val station = sc.textFile("wasb://stations@datagrupo8.blob.core.windows.net/ghcnd-stations.txt").
 map(l=>(l.substring(0,11),
     (l.substring(12,20).trim.toFloat,
     l.substring(21,30).trim.toFloat
        )));
 station.take(10).foreach(println);
 
-val csvs = sc.textFile("wasb://datasets@storagegrupocluster.blob.core.windows.net/*.csv").map(
+//Abre todos os csvs da pasta específicada, os mapeia numa rdd
+val csvs = sc.textFile("wasb://datasets@datagrupo8.blob.core.windows.net/*.csv").map(
 l=>(l.split(",")(0),(
 new Timestamp(l.split(",")(1).substring(0,4).toInt-1900,
               l.split(",")(1).substring(4,6).toInt-1,
               l.split(",")(1).substring(6,8).toInt,0,0,0,0),
-              l.split(",")(2),l.split(",")(3).toShort)));
-
+              l.split(",")(2),l.split(",")(3).toShort
+              )));
+//Produz um dataframe com o conteúdo dos csvs e os dados espaciais das estações
 val staxcsvs = csvs.join(station).map(l=>Register(l._1,l._2._1._1,l._2._1._2,l._2._1._3,l._2._2._1,l._2._2._2)).toDF;
 
+//Salva o resultado em um arquivo parquet
 staxcsvs.take(10).foreach(println);
 staxcsvs.write.save("wasb:///dados.parquet");
 
